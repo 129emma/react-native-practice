42956af756859e82682dd5acdeb72ba437477c11
Emma <129emma@gmail.com>
2018-03-11 14:52:38 +1300

    react native debug, done

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import placesReducers from './reducers/places';
 
 const rootReducer = combineReducers({
@@ -6,8 +6,17 @@ const rootReducer = combineReducers({
 
 });
 
+// add mutiple enhancers in our school
+
+let composeEnhancers = compose;
+
+// dev is speical golobal variable only in react native, it only true if you in develop model
+if(__DEV__){
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-    return createStore(rootReducer);
+    return createStore(rootReducer, composeEnhancers());
 }
 
 export default configureStore;
